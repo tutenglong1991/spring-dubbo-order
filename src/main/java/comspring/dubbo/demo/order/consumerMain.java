c3bf4024ef006578a6638fe081b48ffd4a298d1f
hemeilong <hemeilong@globalegrow.com>
2020-05-25 09:49:23 +0800

    首次上传spring+maven+dubbo搭建rpc远程调框架order-service项目

---
+++ src/main/java/comspring/dubbo/demo/order/consumerMain.java
@@ -0,0 +1,17 @@
+package comspring.dubbo.demo.order;
+
+import com.spring.dubbo.demo.user.api.IUserService;
+import org.apache.dubbo.container.Main;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class consumerMain {
+    public static void main(String[] args) {
+        //第一种方式111
+        ClassPathXmlApplicationContext context=new
+            ClassPathXmlApplicationContext("classpath*:/META-INF/consumer.xml");
+        IUserService iUserService = (IUserService)context.getBean("userService");
+        System.out.println(iUserService.getNameById("1001"));
+        //第二种方式
+//        Main.main(args);
+    }
+}
