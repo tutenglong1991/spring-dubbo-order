13ef7a1d2f19ab0716a27000a2d5e9b73cf2acbd
hemeilong <hemeilong@tuzhanai.com>
2020-06-04 13:05:40 +0800

    修改bean配置错误，导致无法启动consumer服务

--- src/main/java/com/spring/dubbo/demo/order/consumerMain.java
+++ src/main/java/com/spring/dubbo/demo/order/consumerMain.java
@@ -1,6 +1,10 @@
 package com.spring.dubbo.demo.order;
 
+import com.spring.dubbo.demo.order.service.IOrderService;
 import com.spring.dubbo.demo.order.service.impl.OrderServiceImpl;
+import com.spring.dubbo.demo.user.service.api.IUserService;
+import com.spring.dubbo.demo.user.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
@@ -11,10 +15,9 @@ public class consumerMain {
         //第一种方式111
         ClassPathXmlApplicationContext context=new
             ClassPathXmlApplicationContext("classpath*:/META-INF/consumer.xml");
+        IOrderService oderService = (IOrderService)context.getBean("orderService");
         context.start();
-
-        OrderServiceImpl orderService = new OrderServiceImpl();
-        orderService.initOrder("1");
+        oderService.initOrder("1");
         System.in.read(); //阻塞Main线程
         //第二种方式
 //        Main.main(args);
--- src/main/java/com/spring/dubbo/demo/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/spring/dubbo/demo/order/service/impl/OrderServiceImpl.java
@@ -3,16 +3,18 @@ package com.spring.dubbo.demo.order.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.spring.dubbo.demo.order.service.IOrderService;
 import com.spring.dubbo.demo.user.service.api.IUserService;
-import com.spring.dubbo.demo.user.service.impl.UserServiceImpl;
 import com.spring.dubbo.demo.user.service.request.UserQueryReq;
 import com.spring.dubbo.demo.user.service.response.UserRespModel;
+import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
+
 
 @Service
+@Component("orderService")
 public class OrderServiceImpl implements IOrderService {
-
-    IUserService userService = new UserServiceImpl();
+    @Autowired
+    private IUserService userService;
 
     @Override
     public void initOrder(String userId) {
